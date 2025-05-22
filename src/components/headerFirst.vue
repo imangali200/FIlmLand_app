c7ee00a172056d5ae4ea1cf2c3bb43e9f985ad7f
Imangali Zhaksylyk <imangalikazybek1975@gmail.com>
2025-05-22 16:45:58 +0500

    Update headerFirst.vue

--- src/components/headerFirst.vue
+++ src/components/headerFirst.vue
@@ -2,6 +2,7 @@
     <div class="container1">
         <div class="left">
             <router-link to="/">
+                ## Logo
                 <img src="https://github.com/imangali200/FilmLand_app/raw/main/src/assets/IMA%20CINEMA%20LOGO.png" alt="FilmLand Screenshot" width="35px" height="25px" />
             </router-link>
             <div class="option">
